b73ede8fdf77b435ffcfa7daefc8662f0b0153c0
Aman <amanjamad01@gmail.com>
2023-08-22 03:14:30 +0530

    login page

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,10 @@ const dotEnv = require("dotenv");
 const userRoutes = require("./Routes/UserRoutes");
 const cors = require("cors");
 const DataBase = require("./Config/Data");
-
+//
+const multer = require("multer");
+const path = require("path");
+//
 // App.use();
 App.use(express.json());
 dotEnv.config();
@@ -21,6 +24,36 @@ const PORT = process.env.PORT;
 
 App.use("/user", userRoutes);
 
+//
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "uploads"));
+    console.log(req.file);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+App.use(express.static("public"));
+
+App.post("/upload", upload.single("image"), (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No image file uploaded." });
+    }
+    res.status(200).json({ message: "Image uploaded successfully." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error uploading image.", error: error.message });
+  }
+});
+
+//
 App.listen(PORT, () => {
   console.log("server is started");
 });
