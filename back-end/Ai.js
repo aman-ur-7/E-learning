78563197d8c4e3abf044057a8c931aa31fca0520
Aman <amanjamad01@gmail.com>
2023-08-29 22:00:37 +0530

    working wetai

---
+++ back-end/Ai.js
@@ -0,0 +1,7 @@
+// nextAssistantMessage: Empty;
+// trelloApiKey: "82115c63e7d7de753117c6e31b3e1ee4";
+// trelloApiToken: "ATTAfe224044faebb552a52010c01230f81b6c1cb29546d04a2e012dd7c254e3d60626F48C38";
+// trelloCardUrl: Empty;
+// trelloListId: "6463efb01271bca89ddf1751";
+// trelloRequestTitle: "New customer support ticket";
+// tokein : bp_pat_7r5R6OcOiqpMs1qnH2UD7CWZaCrB6iJ5TX4G;
--- vite-project/src/Ai.jsx
+++ vite-project/src/Ai.jsx
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 const Ai = () => {
-  const [query, setQuery] = useState("");
-  const [result, setResult] = useState([]);
-
-  const fetchApi = async () => {
-    const apiKey = "AIzaSyBgLKBYfzZsBzmHIzF_eXO6VGF7DjGSgJM"; // Replace with your API key
-    const searchEngineId = "839b85261ff3d4c95"; // Replace with your custom search engine ID
-
-    try {
-      const response = await axios.get(
-        "https://www.googleapis.com/customsearch/v1",
-        {
-          params: {
-            key: apiKey,
-            cx: searchEngineId,
-            q: query, // Use the user's query
-          },
-        }
-      );
-
-      setResult(response.data.items || []);
-    } catch (error) {
-      console.error("Error fetching search results:", error);
-    }
-  };
-
   return (
     <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Enter search query"
+      <source
+        src="https://mediafiles.botpress.cloud/221cb184-637a-407f-ac92-f14d7856d981/webchat/bot.html"
+        type=""
       />
-      <button onClick={fetchApi}>Fetch Results</button>
-      <ul>
-        {result.map((result) => (
-          <li key={result.cacheId}>{result.title}</li>
-        ))}
-      </ul>
     </div>
   );
 };
