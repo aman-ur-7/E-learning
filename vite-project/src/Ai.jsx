7ba02f7015c7d603f61ad9c68114ac0ba96109be
Aman <amanjamad01@gmail.com>
2023-09-02 11:12:21 +0530

    the commit

--- vite-project/src/Ai.jsx
+++ vite-project/src/Ai.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
+import React from "react";
 
-// const Ai = () => {
-//   return (
-//     <div>
-//       <source
-//         src="https://mediafiles.botpress.cloud/221cb184-637a-407f-ac92-f14d7856d981/webchat/bot.html"
-//         type=""
-//       />
-//     </div>
-//   );
-// };
+const Ai = () => {
+  return (
+    <div>
+      <source
+        src="https://mediafiles.botpress.cloud/221cb184-637a-407f-ac92-f14d7856d981/webchat/bot.html"
+        type=""
+      />
+    </div>
+  );
+};
 
-// export default Ai;
-// // https://wit.ai/
+export default Ai;
+// https://wit.ai/
